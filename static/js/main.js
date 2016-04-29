58bc2758e7ba43cb5118e6bbaf0b0da46ecee8af
Evgeniy Negometyanov <flight.neg@gmail.com>
2016-04-29 23:38:28 +0300

    Main page done

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,17 @@
 (function ($) {
-
+    var carousel = $('.js-carousel');
+    carousel.jcarousel({
+        // btnPrev: '.js-carousel-prev',
+        // btnNext: '.js-carousel-next',
+        // visible: 3
+        wrap: 'circular'
+    });
+    $('.js-carousel-prev').click(function (clickEvent) {
+        clickEvent.preventDefault();
+        carousel.jcarousel('scroll', '-=1');
+    });
+    $('.js-carousel-next').click(function (clickEvent) {
+        clickEvent.preventDefault();
+        carousel.jcarousel('scroll', '+=1');
+    });
 } (jQuery));
